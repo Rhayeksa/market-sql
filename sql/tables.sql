aaeda5acd73d1c78782f62b1abac56532923dc53
rhayeksa <rhayeksads@gmail.com>
2020-12-07 10:50:52 +0700

    fix table

--- sql/tables.sql
+++ sql/tables.sql
@@ -1,57 +1,52 @@
 use
   database_market_sql;
-create
-  table produk (
-    kode_produk varchar(45) not null,
+create table produk (
+    id varchar(45) not null,
     nama varchar(45) not null,
-    stok int not null,
+    stok int not null default 0,
     createdAt datetime not null default current_timestamp(),
-    updateAt datetime not null default current_timestamp(),
-    primary key (kode_produk)
+    updatedAt datetime not null default current_timestamp(),
+    primary key (id)
 );
-create
-  table pemasok (
-    id_pemasok varchar(45) not null,
+create table pemasok (
+    id varchar(45) not null,
     nama varchar(45) not null,
     email varchar(45) not null,
-    phone varchar(45) not null,
+    phone numeric not null,
     alamat varchar(255) not null,
     createdAt datetime not null default current_timestamp(),
-    updateAt datetime not null default current_timestamp(),
-    primary key (id_pemasok)
+    updatedAt datetime not null default current_timestamp(),
+    primary key (id)
 );
-create
-  table pelanggan (
-    id_pelanggan varchar(45) not null,
+create table pelanggan (
+    id varchar(45) not null,
     nama varchar(45) not null,
     email varchar(45) not null,
-    phone varchar(45) not null,
+    phone numeric not null,
     alamat varchar(255) not null,
     createdAt datetime not null default current_timestamp(),
-    updateAt datetime not null default current_timestamp(),
-    primary key (id_pelanggan)
+    updatedAt datetime not null default current_timestamp(),
+    primary key (id)
 );
-create
-  table pembelian (
-    kode_pembelian varchar(45) not null,
+create table pembelian (
+    id varchar(45) not null,
     harga int not null,
     banyak int not null,
-    kode_produk varchar(45) not null,
-    id_pemasok varchar(45) not null,
+    produk_id varchar(45) not null,
+    pemasok_id varchar(45) not null,
     createdAt datetime not null default current_timestamp(),
-    updateAt datetime not null default current_timestamp(),
-    constraint fk_pembelian_produk foreign key (kode_produk) references produk (kode_produk),
-    constraint fk_pembelian_pemasok foreign key (id_pemasok) references pemasok (id_pemasok)
+    updatedAt datetime not null default current_timestamp(),
+    constraint fk_pembelian_produk foreign key (produk_id) references produk (id),
+    constraint fk_pembelian_pemasok foreign key (pemasok_id) references pemasok (id)
 );
-create
-  table penjualan (
-    kode_penjualan varchar(45) not null,
+create table penjualan (
+    id varchar(45) not null,
     harga int not null,
     banyak int not null,
-    kode_produk varchar(45) not null,
-    id_pelanggan varchar(45) not null,
+    produk_id varchar(45) not null,
+    pelanggan_id varchar(45) not null,
     createdAt datetime not null default current_timestamp(),
-    updateAt datetime not null default current_timestamp(),
-    constraint fk_penjualan_produk foreign key (kode_produk) references produk (kode_produk),
-    constraint fk_penjualan_pelanggan foreign key (id_pelanggan) references pelanggan (id_pelanggan)
+    updatedAt datetime not null default current_timestamp(),
+    constraint fk_penjualan_produk foreign key (produk_id) references produk (id),
+    constraint fk_penjualan_pelanggan foreign key (pelanggan_id) references pelanggan (id)
 );
